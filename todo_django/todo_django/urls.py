bb57d5e14560a64497e63dc112c797532b004315
Mateus Schverz <matefs8569@gmail.com>
2024-04-13 22:39:15 -0300

    fix: added "todo" routes

--- todo_django/todo_django/urls.py
+++ todo_django/todo_django/urls.py
@@ -3,15 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-
-from todo.viewsets import TarefaViewSet
-
-
-router = routers.DefaultRouter()
-router.register('tarefas', TarefaViewSet)  # Substitua por views.TarefaViewSet se for o caso
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('todo/', include('todo.urls')),  # Inclua as URLs do aplicativo "todo"
+
 
 ]
